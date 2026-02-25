629319834289c74683d215c0fd236d1e1dc4b102
juanjoseruiz-stack <juanjoseruizcardona2008@gmail.com>
2026-02-25 16:03:09 -0500

    se coloco nombre y operaciones

---
+++ programacion_secuencial.js
@@ -0,0 +1,18 @@
+let nombre = "juan";
+let apellido = "ruiz"; 
+let edad = 20;
+let numero = 30;
+function sumar(a, b) {
+    return a + b;
+}
+
+function restar(a, b) {
+    let operacion = a - b;
+    return operacion
+}
+
+console.log("el resultado de la suma es: " + sumar(numero, edad));
+
+let resultadoResta = restar(5, 7);
+
+console.log("nmbre comleto: " + nombre, "" + apellido);
