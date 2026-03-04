d2bd2319a6ba1af3bd36b0f921d54ce22c1e3c64
juanjoseruiz-stack <juanjoseruizcardona2008@gmail.com>
2026-03-04 15:35:56 -0500

    funcion  divicionde

---
+++ funciones.js
@@ -0,0 +1,11 @@
+// function Suma(x) {
+//     let accion = x + 5;
+//     return accion;
+// };
+// console.log (Suma(7));
+
+function Divicion(x) {
+    let accion = x / 10; 
+    return accion;
+};
+console.log (Divicion(8));
