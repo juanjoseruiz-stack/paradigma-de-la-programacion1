592c7eae9254caf531ffe5e4f362bba4027ba760
juanjoseruiz-stack <juanjoseruizcardona2008@gmail.com>
2026-03-04 16:13:23 -0500

    funciones

--- funciones.js
+++ funciones.js
@@ -1,11 +1,29 @@
-// function Suma(x) {
-//     let accion = x + 5;
-//     return accion;
-// };
-// console.log (Suma(7));
+function Suma(x) {
+    let accion = x + 5;
+    return accion;
+};
+console.log (Suma(7));
 
 function Divicion(x) {
     let accion = x / 10; 
     return accion;
 };
 console.log (Divicion(8));
+
+let resultado = 0 
+function SumarPrecios(a, b)  {
+    return resultado = resultado + a * b;
+    
+};
+console.log (SumarPrecios(4, 6));
+console.log (SumarPrecios(2, 3));
+console.log (SumarPrecios(5, 8));
+
+function Multiplcar(a){
+    return a * a;
+};
+
+function SumaDeLaMultiplicacion(num1, num2) {
+    return Multiplcar(num1) + Multiplcar(num2);
+};
+console.log(SumaDeLaMultiplicacion(3,4));
